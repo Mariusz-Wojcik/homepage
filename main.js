8384c0f1e7567f327a938030fa5c8bfbceaafcf4
Mariusz Wójcik <36dragan@gmail.com>
2020-05-04 23:25:59 +0200

    Formatowanie Prettier'em

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const name = 'Mariusz';
+const name = "Mariusz";
 const age = 18;
 const hook = 22;
 
-console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`)
-console.log(`Hak ma ledwie ${hook} lata ;)`)
+console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+console.log(`Hak ma ledwie ${hook} lata ;)`);
