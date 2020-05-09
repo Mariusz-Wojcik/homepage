d5df20d3dafe1b04444df85d07032a4673e57194
Mariusz Wójcik <36dragan@gmail.com>
2020-05-09 18:33:14 +0200

    fun with innerHTML

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const hook = 22;
 
 console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
 console.log(`Hak ma ledwie ${hook} lata ;)`);
+
+const js_code = document.querySelector('.article-section__title');
+console.log(js_code);
+
+js_code.innerHTML = 'here you are... <div class= "JS_box"> JS is here </div>';
