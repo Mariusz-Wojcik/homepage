fc8441f7dbb9d47dd58c533ebd643debf8890b7b
Mariusz Wójcik <36dragan@gmail.com>
2020-05-16 19:45:21 +0200

    welcome function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const name = "Mariusz";
+const name = 'Mariusz';
 const age = 18;
 const hook = 22;
 
@@ -9,3 +9,17 @@ const js_code = document.querySelector('.article-section__title');
 console.log(js_code);
 
 js_code.innerHTML = 'here you are... <div class= "JS_box"> JS is here </div>';
+
+function welcome(age, name, hook) {
+    console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+    console.log(`Hak ma ledwie ${hook} lata ;)`);
+};
+
+welcome(age, name, hook);
+
+const welcome_new = (age, name, hook)=>{
+    console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+    console.log(`Hak ma ledwie ${hook} lata ;)`);  
+};
+
+welcome_new(age, name, hook);
